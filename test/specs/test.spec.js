118cef52874f50afe8045da24aa46adcaf981c1c
Sylwia Lewera <lewera.sylwia@gmail.com>
2023-02-27 20:55:24 +0100

    first Test Suite with chromeDriver

---
+++ test/specs/test.spec.js
@@ -0,0 +1,8 @@
+describe("Test suite", () => {
+    it("First test", async () =>{
+        await browser.url("https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard");
+        const pageTitle = await browser.getTitle();
+
+        expect(pageTitle).toEqual("Appointment Planner - Syncfusion Angular Components Showcase App");
+    })
+})
